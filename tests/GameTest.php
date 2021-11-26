fbabb6c2d78ee767ff7bf4925e629f9efee1f293
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-11-27 08:36:50 +0900

    テスト雛形作成

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,2 +1,14 @@
 <?php
 namespace BlackJack\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../lib/GameClass.php');
+
+class GameTest extends TestCase
+{
+    public function testGameStart()
+    {
+
+    }
+}
