3925962e9b17d0e8c60005ae50335cf834508a7a
seiichi0404 <yamayamaseisei1834@gmail.com>
2022-01-05 10:33:25 +0900

    ディーラーテスト完了

--- tests/DealerTest.php
+++ tests/DealerTest.php
@@ -29,56 +29,61 @@ class DealerTest extends TestCase
         $drawCards = [
             [
                 'prim' => 'A',
-                'rank' => '1',
+                'rank' => 1,
                 'type' => 'ダイヤ'
             ],
         ];
 
         $drawCards2 = [
             [
-                'prim' => '7',
-                'rank' => '7',
+                'prim' => 'A',
+                'rank' => 1,
+                'type' => 'ダイヤ'
+            ],
+            [
+                'prim' => 'A',
+                'rank' => 1,
                 'type' => 'ダイヤ'
             ],
         ];
 
         $drawCards3 = [
             [
-                'prim' => 'A',
-                'rank' => '1',
-                'type' => 'ダイヤ'
-            ],
-            [
-                'prim' => 'A',
-                'rank' => '1',
-                'type' => 'ダイヤ'
-            ],
-        ];
-
-        $drawCards4 = [
-            [
                 'prim' => '3',
-                'rank' => '3',
+                'rank' => 3,
                 'type' => 'ハート'
             ],
             [
                 'prim' => 'A',
-                'rank' => '1',
+                'rank' => 1,
                 'type' => 'クラブ'
             ],
         ];
 
-        // カードが1枚　Aの時
+        // カードが1枚　マッチポイント以下
         $dealer = new Dealer();
         $dealer->handleScore($dealer, $drawCards);
         $this->assertSame(11, $dealer->getScore());
 
-        // カードが1枚　A以外
-        // $dealer = new Dealer();
-        // $dealer->handleScore($dealer, $drawCards);
-        // $this->assertSame(11, $dealer->getScore);
+        //  カードが1枚　マッチポイント以上
+        $dealer->handleScore($dealer, $drawCards);
+        $this->assertSame(12, $dealer->getScore());
 
+        // カードが2枚 どちらもAの時
+        $dealer2 = new Dealer();
+        $dealer2->handleScore($dealer2, $drawCards2);
+        $this->assertSame(12, $dealer2->getScore());
 
+        // カードが2枚 どちらか一方がAの時
+        $dealer3 = new Dealer();
+        $dealer3->handleScore($dealer3, $drawCards3);
+        $this->assertSame(14, $dealer3->getScore());
+    }
+
+    public function testGetName()
+    {
+        $dealer = new Dealer();
+        $this->assertSame('dealer', $dealer->getName());
     }
 
 }
